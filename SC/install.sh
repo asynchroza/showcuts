afb7cf04dff6579e6da1ff074b37c9670e1dd5e8
michaelbozhilov@gmail.com <michaelbozhilov@gmail.com>
2022-08-26 01:25:55 +0300

    Update install.sh to work for both bash and zsh

--- SC/install.sh
+++ SC/install.sh
@@ -1,7 +1,13 @@
 #!/bin/bash
+if [ "$1" == "--zsh" ]; then
+    RCFILE=.zshrc
+else
+    RCFILE=.bashrc
+fi
 
 ln -s $PWD/sc.py $PWD/sc
-echo "export PATH="\$PATH:$PWD"" >> $HOME/.bashrc
+echo "export PATH="\$PATH:$PWD"" >> $HOME/$RCFILE
+
+echo "Loading the shell so that SC is on PATH"
+echo "Run the tool by typing sc in the terminal"
 exec $SHELL
-
-
--- SC/zinstall.zsh
+++
@@ -1,7 +0,0 @@
-#!/bin/zsh
-
-ln -s $PWD/sc.py $PWD/sc
-echo "export PATH="\$PATH:$PWD"" >> $HOME/.zshrc
-exec $SHELL
-
-
