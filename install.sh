a306d4f7e8c4250df1ff8872aebf5d81855189c0
Michael Bozhilov <mbozhilov-qb@quickbase.com>
2023-06-24 22:22:30 +0300

    Update installation script with ln validation

--- install.sh
+++ install.sh
@@ -1,13 +1,30 @@
 #!/bin/bash
+
 if [ "$(basename $SHELL)" == "zsh" ]; then
     RCFILE=.zshrc
 else
     RCFILE=.bashrc
 fi
 
-ln -s $PWD/sc.py $PWD/sc
-echo "export PATH="\$PATH:$PWD"" >> $HOME/$RCFILE
 
-echo "Loading the shell so that SC is on PATH"
-echo "Run the tool by typing sc in the terminal"
+FILE_PATH=$HOME/$RCFILE
+EXPORT_LINE="export PATH="\$PATH:$PWD""
+
+if grep -Fxq "$EXPORT_LINE" "$FILE_PATH"; then
+    echo "Path to ShowCuts is already exported!"
+    exit 0
+else
+     # Create a symbolic link to sc.py if it doesn't exist
+    if [ ! -L "$PWD/sc" ]; then
+        ln -s "$PWD/sc.py" "$PWD/sc"
+    else
+        echo "ShowCuts is already installed in /usr/bin"
+    fi
+
+    echo $EXPORT_LINE >> $HOME/$RCFILE
+
+    echo "Run the tool by typing sc in the terminal"
+fi
+
 exec $SHELL
+
